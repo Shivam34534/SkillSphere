e5d36269b50222975ec7d8c46061860fd02c45fa
Shivam <shivam34500@gmail.com>
2026-05-06 00:14:55 +0530

    change

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -8,12 +8,16 @@ dotenv.config();
  */
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+  port: parseInt(process.env.EMAIL_PORT) || 587,
+  secure: parseInt(process.env.EMAIL_PORT) === 465, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
+  requireTLS: true
 });
 
 /**
