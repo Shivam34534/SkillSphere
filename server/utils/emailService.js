f91b24bc3fce7d5de378da07c533dcceacadbc42
Shivam <shivam34500@gmail.com>
2026-05-07 01:18:30 +0530

    Switch to SSL Port 465 for email reliability

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -7,7 +7,9 @@ dotenv.config();
  * Reusable Email Service
  */
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
